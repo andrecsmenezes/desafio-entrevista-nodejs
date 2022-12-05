3fb4456055e6523db86d234ae12f70bc777c2e33
André Menezes <andre.menezes@troc.com.br>
2022-12-05 12:22:13 -0300

    test should create a vehicle (happy way)

--- api/src/vehicles/vehicles.controller.spec.ts
+++ api/src/vehicles/vehicles.controller.spec.ts
@@ -1,23 +1,65 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { VehiclesController } from './vehicles.controller';
 import { VehiclesService } from './vehicles.service';
-import { vehiclesProviders } from './vehicles.providers';
-import { DatabaseModule } from '../database.module';
+import { Vehicle } from './entities/vehicle.entity';
+import { CarBrand, CarType, VehicleType } from './vehicles.interface';
+import { CreateVehicleDto } from './dto/create-vehicle.dto';
+import { ModuleMocker, MockFunctionMetadata } from 'jest-mock';
+
+const moduleMocker = new ModuleMocker(global);
 
 describe('VehiclesController', () => {
   let controller: VehiclesController;
-
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [DatabaseModule],
       controllers: [VehiclesController],
-      providers: [...vehiclesProviders, VehiclesService],
-    }).compile();
+    })
+      .useMocker((token) => {
+        if (token === VehiclesService) {
+          const vehicle: CreateVehicleDto = {
+            board: 'AAA0000',
+            brand: CarBrand.FIAT,
+            color: 'BLUE',
+            model: 'UNO MILLE',
+            type: CarType.HATCH,
+            vehicleType: VehicleType.CAR,
+          };
+          const vehicleCreated = new Vehicle();
+          vehicleCreated.id = 1;
+          vehicleCreated.board = vehicle.board;
+          vehicleCreated.brand = vehicle.brand;
+          vehicleCreated.color = vehicle.color;
+          vehicleCreated.model = vehicle.model;
+          vehicleCreated.type = vehicle.type;
+          vehicleCreated.vehicleType = vehicle.vehicleType;
+          return {
+            create: jest.fn().mockResolvedValue(vehicleCreated),
+          };
+        }
+        if (typeof token === 'function') {
+          const mockMetadata = moduleMocker.getMetadata(
+            token,
+          ) as MockFunctionMetadata<any, any>;
+          const Mock = moduleMocker.generateFromMetadata(mockMetadata);
+          return new Mock();
+        }
+      })
+      .compile();
 
     controller = module.get<VehiclesController>(VehiclesController);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  it('should create a vehicle', async () => {
+    const vehicle: CreateVehicleDto = {
+      board: 'AAA0000',
+      brand: CarBrand.FIAT,
+      color: 'BLUE',
+      model: 'UNO MILLE',
+      type: CarType.HATCH,
+      vehicleType: VehicleType.CAR,
+    };
+    const vehicleCreated = Object.assign(new Vehicle(), vehicle);
+    vehicleCreated.id = 1;
+    expect(await controller.create(vehicle)).toStrictEqual(vehicleCreated);
   });
 });
